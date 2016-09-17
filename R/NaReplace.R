8649f113f0a184e8a16db103ddd85e8540276d32
Fangxin Hong <fxhong@jimmy.harvard.edu>
2016-09-17 08:35:48 +0000

    New update - part1
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RankProd@121031 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/NaReplace.R
+++
@@ -1,8 +0,0 @@
-"NaReplace" <-
-function(X){
-    if (is.vector(X)) { X=matrix(X,nrow=1)}     
-
-    for(i in 1:nrow(X))
-        X[i,  ] <- replace(X[i,  ], which(is.na(X[i,  ])), mean(X[i,  ], na.rm = TRUE))
-    return(X)
-}
--- R/RPadvance.R
+++
@@ -1,8 +0,0 @@
-"RPadvance" <-
-function(data,cl,origin,num.perm=100,logged=TRUE,na.rm=FALSE,gene.names=NULL,plot=FALSE, rand=NULL, huge=FALSE)
-{
-  if (huge)
-    return(RPadvanceV2(data,cl,origin,num.perm,logged,na.rm,gene.names,plot,rand))
-  else
-    return(RPadvanceV1(data,cl,origin,num.perm,logged,na.rm,gene.names,plot,rand));
- }
